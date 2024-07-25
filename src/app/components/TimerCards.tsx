9a1de61d8c098cbb80325bdc65ec1c464ef9fc67
Miles <miles.gerber@cdl.co.uk>
2024-07-25 17:24:52 +0100

    cards changing at the correct timings, currently 5 seconds each

--- src/app/components/TimerCards.tsx
+++ src/app/components/TimerCards.tsx
@@ -3,24 +3,18 @@ import Image from 'next/image';
 import ProgressBar from '../components/ProgressBar';
 import data from '../kriya.json';
 
-const TimerCards: React.FC<{ activeCountdown: number; resetActiveCountdown: boolean }> = ({ activeCountdown, resetActiveCountdown }) => {
+const TimerCards: React.FC<{ activeCountdown: number; resetActiveCountdown: () => void }> = ({ activeCountdown, resetActiveCountdown }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    if (data[currentIndex] && activeCountdown >= data[currentIndex].seconds) {
+    const totalSeconds = data[currentIndex].seconds;
+    if (activeCountdown >= totalSeconds) {
       if (currentIndex < data.length - 1) {
         setCurrentIndex(currentIndex + 1);
+        resetActiveCountdown();
       }
     }
-  }, [activeCountdown, currentIndex]);
-
-  // useEffect(() => {
-  //   if (data[currentIndex] && activeCountdown >= data[currentIndex].seconds) {
-  //     if (currentIndex < data.length - 1) {
-  //       setCurrentIndex(currentIndex + 1);
-  //     }
-  //   }
-  // }, [activeCountdown, currentIndex]);
+  }, [activeCountdown, currentIndex, resetActiveCountdown]);
 
   return (
     <>
@@ -28,8 +22,9 @@ const TimerCards: React.FC<{ activeCountdown: number; resetActiveCountdown: bool
         <div key={currentIndex} className="max-w-sm rounded overflow-hidden shadow-lg my-4">
           <Image
             src={data[currentIndex].image}
-            alt='hero image for kriya practice page'
-            width={500} height={300} // You can adjust width and height as needed
+            alt={`hero image for ${data[currentIndex].name}`}
+            width={500}
+            height={300} // Adjust width and height as needed
           />
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{data[currentIndex].name}</div>
@@ -43,6 +38,6 @@ const TimerCards: React.FC<{ activeCountdown: number; resetActiveCountdown: bool
       )}
     </>
   );
-}
+};
 
 export default TimerCards;
