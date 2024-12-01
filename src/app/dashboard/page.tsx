265246de5e5c9121486c7458c34388c65101004e
Miles Felton Gerber <mwfelton03@gmail.com>
2024-12-01 18:36:00 +0000

    UI for testing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+import { useSession } from "next-auth/react";
 import React from 'react'
 
 export default function UserPage() {
+  const { data: session } = useSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
        <h1>user page</h1>
+       <h1>Hi {session?.user?.email}</h1>
       </div>
 
     
